84bd977253cef9d3cd9c4178dddcc87b16e9c664
Auslender <marysok17@gmail.com>
2017-12-13 20:28:11 +0300

    aposterior number of iterations & bifurcations

--- main.cpp
+++ main.cpp
@@ -7,16 +7,8 @@ int main() {
     size_t iterations;
     std::cin >> r;
     std::ofstream os("plot");
-    //std::ifstream is("config");
-    std::cin >> iterations;
-    if (r <= 0)
-        std::cout << "Must be positive\n";
-    for (size_t i = 0; i < iterations; i++) {
-        os << i << " " << get_next_approx(r, i) << std::endl;
-    }
-    if (system("gnuplot first_gnu -p")) {
-        std::cerr << "Can't draw the graph\n";
-    }
+    //get_next_approx(os, r);
+    draw_bifurcation(os);
     os.close();
     return 0;
 }
--- simple_iteration/iterations.h
+++
@@ -1,8 +0,0 @@
-#ifndef MNSC_SIMPLE_ITERATION_H
-#define MNSC_SIMPLE_ITERATION_H
-
-#include <stddef.h>
-
-double get_next_approx(double r, size_t iter);
-
-#endif //MNSC_SIMPLE_ITERATION_H
